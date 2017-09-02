cc614b3912d591dc02384998e998782e1ecc8688
Macharia Marigi <machariamarigi@gmail.com>
2017-09-02 22:32:48 +0300

    [Chore #150734415]Add factory function for our app

---
+++ api/__init__.py
@@ -0,0 +1,28 @@
+"""
+    Main module for the api containing the factory function for the
+    application
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import app_config
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """
+        Factory function that creates instance of the app with a given
+        configuration
+        Args:
+            config_name(str): Name of the configuration to create app
+        Returns:
+            app(obj->Flask): Return instance of the app
+    """
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    return app
