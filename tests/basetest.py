6155de40705010cdabece432a9621d8c71adacf7
Macharia Marigi <machariamarigi@gmail.com>
2017-09-02 21:58:37 +0300

    [#150734415]Add different test files

---
+++ tests/basetest.py
@@ -0,0 +1,6 @@
+from flask_testing import TestCase
+
+
+class TestBase(TestCase):
+    """Base class which other tests will inherit from"""
+    pass
---
+++ tests/test_models.py
@@ -0,0 +1,11 @@
+from tests.basetest import TestBase
+
+
+class APITestCase(TestBase):
+    """Class to test the API"""
+    pass
+
+
+class TestErrors(TestBase):
+    """Test error codes and ensure they return json information"""
+    pass
