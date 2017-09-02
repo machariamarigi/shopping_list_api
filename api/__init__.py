574126b938bc84c951808f731c715a2f1dc8b437
Macharia Marigi <machariamarigi@gmail.com>
2017-09-02 22:35:18 +0300

    [Chore #150734415]Add and register api_v1 blueprint

--- api/__init__.py
+++ api/__init__.py
@@ -25,4 +25,8 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
+
+    from .api_v1 import api as api_v1_blueprint
+    app.register_blueprint(api_v1_blueprint, url_prefix="/api/v1")
+
     return app
---
+++ api/api_v1/__init__.py
@@ -0,0 +1,5 @@
+"""Blueprint for the api"""
+
+from flask import Blueprint
+
+api = Blueprint('api', __name__)
