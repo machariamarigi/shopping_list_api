83098a190059d0de4cfdabff3f43bb74ba025b78
Macharia Marigi <machariamarigi@gmail.com>
2017-09-16 10:24:02 +0300

    [Feature #150795800]Register the API version 1 blueprint

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 """Module containing factory function for the application"""
 from flask import Flask
+from flask_restplus import Api
 
 from config import app_config
+from api_v1 import Blueprint_apiV1
 from api_v1.models import db
+
+
+Api_V1 = Api(
+    app=Blueprint_apiV1,
+    title="Shopping List Api",
+    description="An API for a Shopping List Application",
+    contact="machariamarigi@gmail.com"
+)
 
 
 def create_app(environment):
@@ -11,6 +21,7 @@ def create_app(environment):
     app.config.from_object(app_config[environment])
     db.init_app(app)
 
+    app.register_blueprint(Blueprint_apiV1)
     # add urls here
 
     return app
