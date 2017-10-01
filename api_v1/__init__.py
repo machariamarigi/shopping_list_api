03bc1c0dc5f80414d8dd92319740717837a29f44
Macharia Marigi <machariamarigi@gmail.com>
2017-10-01 14:54:34 +0300

    [Update]Move sh_ns namespace declaration to a api_v1/__init__.py

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -7,4 +7,10 @@ Blueprint_apiV1 = Blueprint('api', __name__, url_prefix="/api/v1")
 auth = Namespace(
     "Auth", description='Operations related to Authentication', path='/auth')
 
+sh_ns = Namespace(
+    "Shoppinglist Endpoints",
+    description="Operations related to Shoppinglists and Items",
+    path="/"
+)
+
 from . import errors
