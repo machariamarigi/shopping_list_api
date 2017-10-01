6459d6d8ef95a813767823de71019b54b00f956e
Macharia Marigi <machariamarigi@gmail.com>
2017-10-01 14:52:52 +0300

    [Update]Add helper functions to be used around the api

--- api_v1/helpers.py
+++ api_v1/helpers.py
@@ -1,5 +1,11 @@
 """This module contains helper functions used in the API"""
+import datetime
 import re
+from functools import wraps
+
+from flask import request
+
+from api_v1.models import User
 
 
 def name_validalidation(name, context):
@@ -11,3 +17,32 @@ def name_validalidation(name, context):
             context: "null"
         }
         return response, 400
+
+
+def datetimeconverter(obj):
+    """Function to convert datime objects to a string"""
+    if isinstance(obj, datetime.datetime):
+        return obj.__str__()
+
+
+def token_required(funct):
+    """Decorator method to check for jwt tokens"""
+    @wraps(funct)
+    def wrapper(*args, **kwargs):
+        """Wrapper function to add pass down results of the token decoding"""
+        if 'Authorization' in request.headers:
+            access_token = request.headers.get('Authorization')
+
+            data = User.decode_token(access_token)
+            if not isinstance(data, str):
+                user_id = data
+            else:
+                response = {
+                    'message': data
+                }
+                return response, 401
+
+            return funct(*args, user_id, **kwargs)
+    wrapper.__doc__ = funct.__doc__
+    wrapper.__name__ = funct.__name__
+    return wrapper
