ed0a18b93b0b99a9deb978427930fed898dc7b81
Macharia Marigi <macharia.marigi@andela.com>
2017-10-08 19:27:45 +0300

    [FIX]Add the sqlite db fallback for tests

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class TestingConfig(Config):
     TESTING = True
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('test_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('test_db') or 'sqlite:///:memory'
 
 
 app_config = {
